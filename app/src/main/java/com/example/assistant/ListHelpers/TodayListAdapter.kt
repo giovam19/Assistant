434d08763f446a0ab12f90615e42516ec2a0a35e
Giovanni Vecchies <giovam19@gmail.com>
2024-07-29 00:17:25 +0200

    Improve in today list

--- app/src/main/java/com/example/assistant/ListHelpers/TodayListAdapter.kt
+++ app/src/main/java/com/example/assistant/ListHelpers/TodayListAdapter.kt
@@ -1,6 +1,7 @@
 package com.example.assistant.ListHelpers
 
-import android.opengl.Visibility
+import android.content.Context
+import android.content.Intent
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -11,9 +12,9 @@ import com.example.assistant.Model.Birthday
 import com.example.assistant.Model.Reminder
 import com.example.assistant.Model.Task
 import com.example.assistant.R
+import com.example.assistant.SoloTaskActivity
 
-
-class ListAdapter(private val items: List<Reminder>) : RecyclerView.Adapter<ListAdapter.ViewHolder>() {
+class TodayListAdapter(private val items: List<Reminder>, val context: Context) : RecyclerView.Adapter<TodayListAdapter.ViewHolder>() {
 
     class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         val icon: ImageView = itemView.findViewById(R.id.itemImage)
@@ -22,8 +23,12 @@ class ListAdapter(private val items: List<Reminder>) : RecyclerView.Adapter<List
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        val view = LayoutInflater.from(parent.context).inflate(R.layout.today_item, parent, false)
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.item_list_today, parent, false)
         return ViewHolder(view)
+    }
+
+    override fun getItemCount(): Int {
+        return items.size
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
@@ -32,14 +37,16 @@ class ListAdapter(private val items: List<Reminder>) : RecyclerView.Adapter<List
             holder.icon.setImageResource(R.drawable.task_icon2)
             holder.name.text = item.title
             holder.hour.text = item.hour
+
+            holder.itemView.setOnClickListener {
+                val intent = Intent(context, SoloTaskActivity::class.java)
+                intent.putExtra("uuid", items[position].id)
+                context.startActivity(intent)
+            }
         } else if (item is Birthday) {
             holder.icon.setImageResource(R.drawable.birthday_ic1)
             holder.name.text = "Felicita a ${item.name}"
-            holder.hour.visibility = TextView.INVISIBLE
+            holder.hour.visibility = TextView.GONE
         }
-    }
-
-    override fun getItemCount(): Int {
-        return items.size
     }
 }
